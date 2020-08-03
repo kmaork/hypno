cbcf2af61b5a45c3f3008cf38d810738f2c128d0
Maor Kleinberger <kmaork@gmail.com>
2020-08-04 01:03:00 +0300

    allowing to pass str python code

--- hypno/hypno.py
+++ hypno/hypno.py
@@ -1,4 +1,6 @@
 from importlib.util import find_spec
+from typing import AnyStr
+
 from pyinjector import inject
 from os import urandom, unlink
 from shutil import copy
@@ -10,7 +12,9 @@ DATA_DIR.mkdir(parents=True, exist_ok=True)
 injection_lib_path = Path(find_spec('.injection', __package__).origin)
 
 
-def inject_py(pid: int, python_code: bytes) -> None:
+def inject_py(pid: int, python_code: AnyStr) -> None:
+    if isinstance(python_code, str):
+        python_code = python_code.encode()
     copied_lib = DATA_DIR / f'{urandom(3).hex()}-{python_code.hex()}'
     copy(injection_lib_path, copied_lib)
     inject(pid, str(copied_lib))
