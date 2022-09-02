c0cc831009e732a4243cbcb58c771de0cb834cf2
Maor Kleinberger <kmaork@gmail.com>
2022-09-03 01:21:12 +0300

    Use AddPendingCall for injection

--- hypno/injection.c
+++ hypno/injection.c
@@ -9,12 +9,15 @@ PyMODINIT_FUNC PyInit_injection(void) {return NULL;}
 
 char PYTHON_CODE[MAX_PYTHON_CODE_SIZE + 1] = "\0--- hypno code start ---" STR(MAX_PYTHON_CODE_SIZE);
 
-void run_python_code() {
+int run_python_code(char *code) {
+    PyRun_SimpleString(code);
+    return 0;
+}
+
+void inject_python() {
     if (PYTHON_CODE[0]) {
         int saved_errno = errno;
-        PyGILState_STATE gstate = PyGILState_Ensure();
-        PyRun_SimpleString(PYTHON_CODE);
-        PyGILState_Release(gstate);
+        Py_AddPendingCall(&run_python_code, &PYTHON_CODE);
         errno = saved_errno;
     }
 }
@@ -25,7 +28,7 @@ void run_python_code() {
     BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
         switch( fdwReason ) {
             case DLL_PROCESS_ATTACH:
-                run_python_code();
+                inject_python();
                 return FALSE;
                 break;
         }
@@ -34,6 +37,6 @@ void run_python_code() {
 #else
     __attribute__((constructor))
     static void init(void) {
-        run_python_code();
+        inject_python();
     }
 #endif
