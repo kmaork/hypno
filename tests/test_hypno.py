64c4429df3361b1678bf6f23d6faf045a5509a1e
Maor Kleinberger <kmaork@gmail.com>
2023-07-13 13:17:18 +0300

    Add test for multiple injections (#21)

--- tests/test_hypno.py
+++ tests/test_hypno.py
@@ -2,6 +2,8 @@ import sys
 from subprocess import Popen, PIPE
 from pathlib import Path
 import pytest
+from pytest import mark
+
 from hypno import inject_py, CodeTooLongException
 from time import sleep
 
@@ -11,7 +13,8 @@ PROCESS_WAIT_TIMEOUT = 1.5
 WAIT_FOR_PYTHON_SECONDS = 0.75
 
 
-def test_hypno():
+@mark.parametrize('times', [1, 2, 3])
+def test_hypno(times: int):
     # In new virtualenv versions on Windows, python.exe invokes the original python.exe as a subprocess, so the
     # injection does not affect the target python process.
     python = getattr(sys, '_base_executable', sys.executable)
@@ -19,9 +22,11 @@ def test_hypno():
     process = Popen([python, str(WHILE_TRUE_SCRIPT)], stdin=PIPE, stdout=PIPE)
     try:
         sleep(WAIT_FOR_PYTHON_SECONDS)
-        inject_py(process.pid, b'print("' + data + b'", end=""); __import__("__main__").should_exit = True')
+        for _ in range(times - 1):
+            inject_py(process.pid, b'print("' + data + b'", end="");')
+        inject_py(process.pid, b'__import__("__main__").should_exit = True')
         assert process.wait(PROCESS_WAIT_TIMEOUT) == 0
-        assert process.stdout.read() == data
+        assert process.stdout.read() == data * (times - 1)
     finally:
         process.kill()
 
