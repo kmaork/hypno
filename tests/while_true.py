78ab4ae6e5f20cdd2060b7883faf830261be6fd5
Maor Kleinberger <kmaork@gmail.com>
2024-04-19 13:56:35 +0300

    Fix for 3.8

--- tests/while_true.py
+++ tests/while_true.py
@@ -4,5 +4,9 @@ import sys
 should_exit = False
 
 while not should_exit:
-    time.sleep(0.01)
     sys.stderr.write(sys.argv[1])
+    sys.stderr.flush()
+    time.sleep(0.01)
+
+sys.stderr.write(sys.argv[2])
+sys.stderr.flush()
