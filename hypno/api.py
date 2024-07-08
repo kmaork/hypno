2b602bce3554a1b645d7c165cae127c61f241f54
Russ Webber <russ.webber@greenroomrobotics.com>
2024-07-08 18:18:09 +1000

    feat: inject script path instead of code

--- hypno/api.py
+++ hypno/api.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 INJECTION_LIB_PATH = Path(find_spec('.injection', __package__).origin)
 MAGIC = b'--- hypno code start ---'
+MAGIC2 = b'--- hypno script path start ---'
 WINDOWS = sys.platform == 'win32'
 
 
@@ -44,6 +45,41 @@ def inject_py(pid: int, python_code: AnyStr, permissions=0o644) -> None:
             temp.write(python_code)
             temp.write(b'\0')
             temp.write(lib[code_addr + len(python_code) + 1:])
+        path.chmod(permissions)
+        inject(pid, str(temp.name), uninject=True)
+    finally:
+        if path is not None and path.exists():
+            path.unlink()
+
+def inject_py_script(pid: int, python_script: Path, permissions=0o644) -> None:
+    """
+    :param pid: PID of target python process
+    :param python_script: Path to python script to inject to the target process.
+    :param permissions: Permissions of the generated shared library file that will be injected to the target process.
+                        Make sure the file is readable from the target process. By default, all users can read the file.
+    """
+
+    script_path_null_terminated = str(python_script).encode() + b'\0'
+
+    lib = INJECTION_LIB_PATH.read_bytes()
+    magic_addr = lib.find(MAGIC2)
+    path_str_addr = magic_addr - 1
+    # max_size_addr = magic_addr + len(MAGIC2)
+    # max_size_end_addr = lib.find(b'\0', max_size_addr)
+    # max_size = int(lib[max_size_addr:max_size_end_addr])
+
+    # if len(script_path_null_terminated) => max_size:
+    #     raise CodeTooLongException(script_path_null_terminated, max_size)
+
+    patched_lib = bytearray(lib)
+    patched_lib[path_str_addr:(path_str_addr + len(script_path_null_terminated))] = script_path_null_terminated
+
+    path = None
+    try:
+        # delete=False because can't delete a loaded shared library on Windows
+        with NamedTemporaryFile(prefix='hypno', suffix=INJECTION_LIB_PATH.suffix, delete=False) as temp:
+            path = Path(temp.name)
+            temp.write(patched_lib)
         path.chmod(permissions)
         inject(pid, str(temp.name), uninject=True)
     finally:
--- hypno/injection.c
+++ hypno/injection.c
@@ -1,5 +1,6 @@
 #include <Python.h>
 #include <errno.h>
+#include <linux/limits.h>
 
 #define MAX_PYTHON_CODE_SIZE 60500
 #define STR_EXPAND(tok) #tok
@@ -8,12 +9,20 @@
 PyMODINIT_FUNC PyInit_injection(void) {return NULL;}
 
 volatile char PYTHON_CODE[MAX_PYTHON_CODE_SIZE + 1] = "\0--- hypno code start ---" STR(MAX_PYTHON_CODE_SIZE);
+volatile char PYTHON_SCRIPT_PATH[PATH_MAX] = "\0--- hypno script path start ---" STR(PATH_MAX);
 
 void run_python_code() {
     if (PYTHON_CODE[0]) {
         int saved_errno = errno;
         PyGILState_STATE gstate = PyGILState_Ensure();
-        PyRun_SimpleString(PYTHON_CODE);
+        if (PYTHON_CODE[0] != '\0') {
+            PyRun_SimpleString(PYTHON_CODE);
+        } else if (PYTHON_SCRIPT_PATH[0] != '\0') {
+            int fp = fopen(PYTHON_SCRIPT_PATH, "r");
+            PyRun_SimpleFile(fp, PYTHON_SCRIPT_PATH);
+            fclose(fp);
+        }
+
         PyGILState_Release(gstate);
         errno = saved_errno;
     }
